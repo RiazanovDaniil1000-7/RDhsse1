d94ce2b26717661a8ca55040939c979383491f68
RiazanovDaniil <drlitsey2@gmail.com>
2025-09-22 12:19:10 +0300

    Создаем MainClass с методом точки входа

---
+++ src/main/java/com/mipt/MainClass.java
@@ -0,0 +1,15 @@
+package com.mipt;
+
+public class MainClass {
+
+  private int i;
+  private String s;
+  protected double d;
+  public long l = 5;
+
+  public static void main(String[] args) {
+    for (int j = 0; j < 15; j++) {
+      System.out.println("Iter: " + j);
+    }
+  }
+}
