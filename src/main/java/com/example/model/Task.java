89df12169c185536abe3828735832c4c094b3213
user <drlitsey2@gmail.com>
2026-02-27 11:49:36 +0300

    Создание класса Task

---
+++ src/main/java/com/example/model/Task.java
@@ -0,0 +1,81 @@
+package com.example.model;
+
+import java.util.Objects;
+
+
+public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task(int id, String title, String description, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        return id == task.id &&
+            completed == task.completed &&
+            Objects.equals(title, task.title) &&
+            Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, completed);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            ", completed=" + completed +
+            '}';
+    }
+}
